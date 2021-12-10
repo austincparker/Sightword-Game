e3154529bdfeb3cb928d52816f86f532ef345637
Austin Parker <austincparker@gmail.com>
2021-12-09 18:58:21 -0600

    got score and current working

--- src/components/SingleWord.js
+++ src/components/SingleWord.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function SingleWord({ list }) {
+export default function SingleWord({ word }) {
   return (
     <div className="container play-card text-center p-5">
-      <h1 className="display-1">{list.word_1}</h1>
+      <h1 className="display-1">{word}</h1>
     </div>
   );
 }
 
 SingleWord.defaultProps = {
-  list: {},
+  // list: {},
+  word: '',
 };
 SingleWord.propTypes = {
-  list: PropTypes.shape(PropTypes.obj),
+  // list: PropTypes.shape(PropTypes.obj),
+  word: PropTypes.string,
 };
